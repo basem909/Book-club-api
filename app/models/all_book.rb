a92f6c4b93732d3e24a1c83c97580102f7910285
fabianofrank <fabianofrank@protonmail.com>
2022-10-17 17:08:16 -0300

    Add All Books migration

---
+++ app/models/all_book.rb
@@ -0,0 +1,3 @@
+class AllBook < ApplicationRecord
+  belongs_to :user
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
+
+  namespace :api do
+    namespace :v1 do 
+      resources :all_books, only: [:index, :show, :create]
+      resources :users, param: :username, only: [:show]
+    end
+  end
 end
